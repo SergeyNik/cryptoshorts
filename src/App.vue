6a0450ea87657ec18afa1e414b3f719cdbc8f654
snikolchikov <nikolchikov@gmail.com>
2025-05-14 13:48:04 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HeaderNavBar from './components/HeaderNavBar.vue'
+import MainSwiper from './components/MainSwiper.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HeaderNavBar msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <MainSwiper />
+  </main>
+
+  <footer>
+    <div class="wrapper">
+      <HeaderNavBar msg="You did it!" />
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainSwiper.vue
@@ -0,0 +1,59 @@
+<template>
+  <swiper
+      :modules="[Autoplay]"
+      :loop="true"
+      direction="vertical"
+      :autoplay="{ delay: 2000, disableOnInteraction: false }"
+      class="my-swiper"
+  >
+    <swiper-slide v-for="(card, index) in cards" :key="index">
+      <div class="card">
+        <h3>{{ card.title }}</h3>
+        <p>{{ card.description }}</p>
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  setup() {
+    const cards = [
+      { title: 'Карточка 1', description: 'Описание 1' },
+      { title: 'Карточка 2', description: 'Описание 2' },
+      { title: 'Карточка 3', description: 'Описание 3' },
+      { title: 'Карточка 4', description: 'Описание 4' },
+    ];
+
+    return {
+      cards,
+      Autoplay
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-swiper {
+  height: 90vh;
+  width: 100%;
+}
+
+.card {
+  height: 100%;
+  background: #444;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
